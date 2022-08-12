02d5afa22edf124b481dfa1e5adddd8866b7baef
Anna <a.falkonia@gmail.com>
2022-08-12 10:30:48 +0300

    Minor style fixes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.scss'
-import '@fontsource/inter';
+import './styles/global.scss'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
